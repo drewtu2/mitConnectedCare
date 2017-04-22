b2f7b9341ecd09d7f203eae935438c16215eb24f
Andrew Tu <drewtu2@yahoo.com>
2017-04-22 15:57:37 -0400

    Made it possible to serve up imagges from relative paths

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/images/<filename>/')
+def getImg(filename):
+  return app.send_static_file('images/' + filename)
