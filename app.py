46c82c67c8052cf076ed5649bfd4fc3ffcb2b4ba
Andrew Tu <drewtu2@yahoo.com>
2017-04-22 18:14:54 -0400

    updated a bunch of stuff

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,3 +10,13 @@ def index():
 @app.route('/images/<filename>/')
 def getImg(filename):
   return app.send_static_file('images/' + filename)
+
+@app.route('/dashboard/', methods=['GET'])
+def dashboard():
+  if "command" in request.args:
+    command = request.args['command']
+    sendCommand(command)
+  return render_template('dashboard.html')
+
+def sendCommand(command):
+  socketio.emit("channel-a", command)
