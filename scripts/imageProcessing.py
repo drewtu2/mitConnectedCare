8ebcc972c5382af8f2514c3278a487caf9cbb5ab
Paul <paullangton4@gmail.com>
2017-04-22 22:54:22 -0400

    facial recognition via flask. hillary didnt have THIS on the campaign trail folks

--- scripts/imageProcessing.py
+++ scripts/imageProcessing.py
@@ -1,18 +1,25 @@
+#Author: Paul Langton
+import sys
+sys.path.append("../")
+
 import urllib
 import kairos_face
 import time
 import json
+import app
 from PIL import Image
 
-output = "../static/data/facebank/"
-latestImage = "../static/resources/latestStill.jpg"
-paul = "../static/resources/PaulTemplate.jpg"
-andrew = "../static/resources/AndrewTemplate.jpg"
-mark = "../static/resources/MarkTemplate.jpg"
-nick = "../static/resources/NickTemplate.jpg"
-federico = "../static/resources/FedericoTemplate.jpg"
-meg = "../static/resources/MegTemplate.jpg"
+# paths to templates
+output = "static/data/facebank/"
+latestImage = "static/resources/latestStill.jpg"
+paul = "static/resources/PaulTemplate.jpg"
+andrew = "static/resources/AndrewTemplate.jpg"
+mark = "static/resources/MarkTemplate.jpg"
+nick = "static/resources/NickTemplate.jpg"
+federico = "static/resources/FedericoTemplate.jpg"
+meg = "static/resources/MegTemplate.jpg"
 
+# adds faces
 def initAPI():
     kairos_face.enroll_face(file=paul, subject_id='Paul', gallery_name='nuvision')
     kairos_face.enroll_face(file=mark, subject_id='Mark', gallery_name='nuvision')
@@ -22,18 +29,23 @@ def initAPI():
     kairos_face.enroll_face(file=meg, subject_id='Meg', gallery_name='nuvision')
     print "API init successful."
 
-def init():
+# initializes api keys
+def init(doAPI):
     kairos_face.settings.app_id = "e5746d4b"
     kairos_face.settings.app_key = "229f51c0bdccd36001103cf989c56fbe"
-    #initAPI()
+    if doAPI:
+        initAPI()
 
+# gets latest camera image
 def latestStill(address):
     resource = urllib.urlopen(address)
     print("Retrieving latest image...")
     with open(latestImage, 'wb') as output:
         output.write(resource.read())
 
-def processImage(image):
+# process, recognize, crop, and bank images. sends command with person data to
+# flask app
+def processImage():
     latestStill("http://192.168.1.9:88/cgi-bin/CGIProxy.fcgi?cmd=snapPicture2&usr=nuvision&pwd=nuvision")
     recognized_faces = ""
     try:
@@ -46,12 +58,8 @@ def processImage(image):
         still = Image.open(latestImage)
         banked = still.crop((topLeftX, topLeftY, topLeftX + width, topLeftY + height))
         banked.save(output + person + "Banked.jpg")
+        app.sendCommand({"command":"new_bank", "data":(person, output + person + "Banked.jpg")})
         print data
         print "Recognized " + person + ". Hi " + person + "!"
     except:
         print "No faces recognized."
-
-init()
-while(True):
-    time.sleep(1.5)
-    processImage(latestImage)
